3cb886635decfa73953e13cdf4ff0d651876703f
clara23 <clarahuanaco20@gmail.com>
2023-01-27 14:16:04 -0500

    actulisacion04

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule ,FormBuilder  } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        
+        
       ],
       declarations: [
         AppComponent
@@ -20,16 +26,61 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'login-testing'`, () => {
+  it(`should have as title 'Formulario de Pruebas Unitarias'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('login-testing');
+    expect(app.title).toEqual('Formulario de Pruebas Unitarias');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('login-testing app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Formulario de Pruebas Unitarias');
   });
-});
+  it('Should return valid form',()=>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance; 
+    fixture.detectChanges(); 
+      const usuario=app.formLogin.controls['usuario'];
+      const password=app.formLogin.controls['password'];
+      
+       usuario.setValue('mabel23@gmail.com');
+       password.setValue('mabel05');
+
+       const miboton=fixture.debugElement.query(By.css('button'))
+       miboton.nativeElement.click();
+
+        const datatest='loguedo';
+
+        //expect(app.ischeck).toEqual(datatest);
+        expect(app.formLogin.invalid).toBeFalsy();  
+     });
+  
+  
+
+     it('Should return invalid form',()=>{
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance; 
+      fixture.detectChanges(); 
+        const usuario=app.formLogin.controls['usuario'];
+        const password=app.formLogin.controls['password'];
+        
+         usuario.setValue('');
+         password.setValue('');
+  
+        const miboton=fixture.debugElement.query(By.css('button'))
+         miboton.nativeElement.click();
+  
+        
+          expect(app.formLogin.invalid).toBeTruthy();  
+       });
+
+    });
+     
+ 
+      
+  
+
+
+ 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,59 @@
 import { Component ,OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators ,FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  
-  title = 'login-testing';
-  resultado!:string;
+
+  shoeEmoji:boolean=false
+  title = 'Formulario de Pruebas Unitarias';
+ // ischeck:any;
+  resultado:string='';
   usuario:string=''
   contra:string=''
+  submitted = false;
 
   public formLogin!: FormGroup;
 
   constructor(private formBuilder:FormBuilder){
+    this.formLogin = new FormGroup({
+      usuario:new FormControl(),
+      password:new FormControl(),
+    })
 
   }
 
   ngOnInit(): void {
     this.formLogin = this.formBuilder.group({
-      usuario:['', 
-        [
-          Validators.required,
-          Validators.minLength(3),
-          
-        ]
-      ],
-      password:['', 
-        [
-          Validators.required,
-          Validators.minLength(4),
-         
-        ]
-      ]
-    
+      usuario:['',[Validators.required,Validators.minLength(3),Validators.email]],
+      password:['',[Validators.required,Validators.minLength(5),]]
     })
-  
 }
 login(): any{
   console.log(this.formLogin.value)
-  if(this.formLogin.value){
-   
-  this.resultado= 'se ha iniciado sesion correctamente'
-  }else{
-   this.resultado= 'formulario vacio'
+ if (this.formLogin.invalid) {
+  this.resultado='Datos incorrectos'
+  return;
+}else{
+  this.resultado='Inicio de sesion exitosa';
+  return this.resultado;
+
+
+
+}
+
+
+
+
+
+
+    
   }
 
-}
+  
 
 }
+
+
